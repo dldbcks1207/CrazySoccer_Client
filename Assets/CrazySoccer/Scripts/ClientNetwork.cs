3c36194b05c8548b36d58ac3f52080b9c3b9880b
dldbcks1207 <100991462+dldbcks1207@users.noreply.github.com>
2026-06-12 17:35:17 +0900

    feat: 세레머니 추가 & 리팩토링

--- Assets/CrazySoccer/Scripts/ClientNetwork.cs
+++ Assets/CrazySoccer/Scripts/ClientNetwork.cs
@@ -3,9 +3,11 @@ using System.Net.Sockets;
 using System.Text;
 using System;
 using System.Collections.Concurrent;
+using System.IO;
 
 public class ClientNetwork : MonoBehaviour
 {
+    private NetworkStream networkStream;
     private TcpClient socket;
     private InputSystem_Actions inputActions;
     private float lastSentHorizontal = 0f;
@@ -27,7 +29,12 @@ public class ClientNetwork : MonoBehaviour
     {
         socket = new TcpClient();
         Debug.Log("서버 연걸 시도");
-        socket.BeginConnect("127.0.0.1", 9000, OnConnect, null);
+        socket.BeginConnect("127.0.0.1", 9000, (ar) =>
+        {
+            socket.EndConnect(ar);
+            networkStream = socket.GetStream();
+            ReceiveLoop();
+        }, null);
     }
 
     void OnEnable()
@@ -40,24 +47,79 @@ public class ClientNetwork : MonoBehaviour
         inputActions.Disable();
     }
 
-    private void OnConnect(IAsyncResult ar)
-    {
-        socket.EndConnect(ar);
-        Debug.Log("서버와 연결되었습니다!");
-
-        ReceiveLoop();
-    }
-
     private void ReceiveLoop()
     {
         if (socket == null || !socket.Connected) return;
 
-        NetworkStream stream = socket.GetStream();
-        byte[] buffer = new byte[18];
-
-        stream.BeginRead(buffer, 0, buffer.Length, OnReadComplete, new object[] { stream, buffer });
+        NetworkStream stream = networkStream;
+        byte[] headerBuffer = new byte[4];
+        //stream.BeginRead(buffer, 0, buffer.Length, OnReadComplete, new object[] { stream, buffer });
+        stream.BeginRead(headerBuffer, 0, headerBuffer.Length, OnReadHeader, headerBuffer);
     }
 
+    private void OnReadHeader(IAsyncResult ar)
+    {
+        try
+        {
+            NetworkStream stream = networkStream;
+            byte[] headerBuffer = (byte[])ar.AsyncState;
+
+            int bytesRead = stream.EndRead(ar);
+            if (bytesRead == 0) return;
+
+            short packetSize = BitConverter.ToInt16(headerBuffer, 0);
+            PacketType packetType = (PacketType)BitConverter.ToInt16(headerBuffer, 2);
+
+            byte[] bodyBuffer = new byte[packetSize - 4];
+
+            stream.BeginRead(bodyBuffer, 0, bodyBuffer.Length, OnReadBody, new object[] { bodyBuffer, packetType });
+        }
+        catch (Exception ex) { Debug.LogError($"헤더 수신 에러: {ex.Message}"); }
+    }
+
+    private void OnReadBody(IAsyncResult ar)
+    {
+        try
+        {
+            NetworkStream stream = networkStream;
+            object[] state = (object[])ar.AsyncState;
+            byte[] bodyBuffer = (byte[])state[0];
+            PacketType packetType = (PacketType)state[1];
+
+            int bytesRead = stream.EndRead(ar);
+            if (bytesRead == 0) return;
+
+            using (MemoryStream ms = new MemoryStream(bodyBuffer))
+            using (BinaryReader br = new BinaryReader(ms))
+            {
+                switch (packetType)
+                {
+                    case PacketType.SyncPosition:
+                        float playerX = br.ReadSingle(); float playerY = br.ReadSingle();
+                        float ballX = br.ReadSingle(); float ballY = br.ReadSingle();
+
+                        mainThreadQueue.Enqueue(() =>
+                        {
+                            playerTargetPosition = new Vector2(playerX, playerY);
+                            soccerBallTargetPosition = new Vector2(ballX, ballY);
+                        });
+                        break;
+                    case PacketType.GoalEvent:
+                        short scoredTeam = br.ReadInt16();
+
+                        mainThreadQueue.Enqueue(() =>
+                        {
+                            Debug.Log($"Goal {scoredTeam}");
+                        });
+                        break;
+                }
+            }
+
+            ReceiveLoop();
+        }
+        catch (Exception ex) { Debug.LogError($"Error: {ex.Message}"); }
+    }
+    /*
     private void OnReadComplete(IAsyncResult ar)
     {
         object[] state = (object[])ar.AsyncState;
@@ -88,6 +150,7 @@ public class ClientNetwork : MonoBehaviour
             Debug.Log($"Error: {ex.Message}");
         }
     }
+    */
 
     void Update()
     {
@@ -103,7 +166,7 @@ public class ClientNetwork : MonoBehaviour
         float moveDeltaX = soccerBall.position.x - ballPrevPos.x;
         float rotationAmount = moveDeltaX * ballRotationSpeed;
         soccerBall.Rotate(Vector3.forward, -rotationAmount);
-        
+
         if (socket == null || !socket.Connected) return;
 
         float currentHorizontal = inputActions.Player.Move.ReadValue<Vector2>().x;
@@ -116,7 +179,7 @@ public class ClientNetwork : MonoBehaviour
             myInput.IsJump = currentJump;
 
             byte[] packetBytes = myInput.Serialize();
-            NetworkStream stream = socket.GetStream();
+            NetworkStream stream = networkStream;
             stream.Write(packetBytes, 0, packetBytes.Length);
 
             lastSentHorizontal = currentHorizontal;
